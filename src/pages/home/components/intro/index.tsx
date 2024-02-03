4da830e25cfa2746a6ef45836f64e1e5318bab27
luiabdiel <luiggiabdieldev@gmail.com>
2024-02-03 14:36:49 -0300

    feat: intro component in home page

---
+++ src/pages/home/components/intro/index.tsx
@@ -0,0 +1,11 @@
+import { IntroContainer, IntroContent } from './styles'
+
+export function Intro() {
+  return (
+    <IntroContainer>
+      <IntroContent className="container">
+        <p>Intro</p>
+      </IntroContent>
+    </IntroContainer>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,10 @@
+import { Intro } from './components/intro'
+import { HomeContainer } from './styles'
+
 export function HomePage() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <Intro />
+    </HomeContainer>
+  )
 }
