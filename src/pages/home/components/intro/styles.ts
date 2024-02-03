6c52e300ee59a0478cdecf8b94e3c4f611b1090a
luiabdiel <luiggiabdieldev@gmail.com>
2024-02-03 14:50:01 -0300

    feat: add background and install polished lib

--- src/pages/home/components/intro/styles.ts
+++ src/pages/home/components/intro/styles.ts
@@ -1,8 +1,20 @@
 import styled from 'styled-components'
+import introBackGroundImg from '../../../../assets/Background.png'
+import { rgba } from 'polished'
 
 export const IntroContainer = styled.section`
   width: 100%;
   height: 34rem;
+
+  background: ${({
+    theme,
+  }) => `url(${introBackGroundImg}) no-repeat center, linear-gradient(
+        0deg,
+        ${theme.colors['base-white']} 0%,
+        ${rgba(theme.colors['base-background'], 0.2)} 50%,
+        ${theme.colors['base-background']} 100%
+      )`};
+  background-size: cover;
 `
 
 export const IntroContent = styled.div`
