9eff18f79d0cb57521df1855d889405e39b64e12
luiabdiel <luiggiabdieldev@gmail.com>
2024-02-04 13:41:01 -0300

    feat: dynamic component for information with icons

--- src/pages/home/components/intro/index.tsx
+++ src/pages/home/components/intro/index.tsx
@@ -1,7 +1,18 @@
+import { RegularText } from '../../../../components/Typography'
 import { Coffe } from '../../../../components/icons/coffe'
-import { IntroContainer, IntroContent, IntroTitle } from './styles'
+import { InfoWithIcon } from '../../../../components/infoWithIcon'
+import { ShoppingCart } from 'phosphor-react'
+import {
+  BenefitsContainer,
+  IntroContainer,
+  IntroContent,
+  IntroTitle,
+} from './styles'
+import { useTheme } from 'styled-components'
 
 export function Intro() {
+  const { colors } = useTheme()
+
   return (
     <IntroContainer>
       <IntroContent className="container">
@@ -10,7 +21,21 @@ export function Intro() {
             <IntroTitle size="xl">
               Encontre o café perfeito para qualquer hora do dia
             </IntroTitle>
+            <RegularText size="l" color="subtitle" as="h3">
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </RegularText>
           </section>
+          <BenefitsContainer>
+            <InfoWithIcon
+              icon={<ShoppingCart weight="fill" />}
+              text="Compra simples e segura"
+              iconBg={colors['brand-yellow-dark']}
+            />
+            <p>Embalagem mantém o café intacto</p>
+            <p>Entrega rápida e rastreada</p>
+            <p>O café chega fresquinho até você</p>
+          </BenefitsContainer>
         </div>
         <Coffe />
       </IntroContent>
