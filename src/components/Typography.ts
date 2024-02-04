295ce7dc9a5b59407ee975d3b29e6cab9de75ae7
luiabdiel <luiggiabdieldev@gmail.com>
2024-02-03 22:46:31 -0300

    fix

--- src/components/Typography.ts
+++ src/components/Typography.ts
@@ -25,7 +25,7 @@ export const RegularText = styled.p<RegularTextProps>`
   color: ${({ theme, color }) => theme.colors[`base-${color ?? 'title'}`]};
   font-size: ${({ theme, size }) =>
     theme.textSizes[`text-regular-${size ?? 'm'}`]};
-  font-family: ${({ theme }) => theme.fonts.title};
+  font-family: ${({ theme }) => theme.fonts.regular};
   line-height: 130%;
   font-weight: ${({ weight }) => weight ?? 400};
 `
